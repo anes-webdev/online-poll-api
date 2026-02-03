49b6c8e00296d3d8528c33a75c54b8c274f6d623
anes-webdev <anes.webdev@gmail.com>
2026-02-03 12:58:51 +0330

    fix: update title max length

--- src/main/java/com/example/onlinepoll/model/Poll.java
+++ src/main/java/com/example/onlinepoll/model/Poll.java
@@ -20,7 +20,7 @@ public class Poll {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Length(min = 3, max = 20, message = "title length should be between 3 and 20")
+    @Length(min = 3, max = 40, message = "title length should be between 3 and 20")
     private String title;
 
     @Length(min = 5, message = "description length should be at least 5")
